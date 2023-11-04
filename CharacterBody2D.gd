4a63ce3801503bf12aff03ee577a9861fb3f45bc
Goshko812 <goshko812@gmail.com>
2023-11-05 00:24:48 +0200

    still trying to make those bullets work i fucking hate godot4

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -4,8 +4,10 @@ extends CharacterBody2D
 @export var Bullet : PackedScene
 
 func shoot():
-	var b = Bullet
-	add_child(b)
+	var bullet = Bullet.instance() # Load the bullet scene
+	get_tree().root.add_child(bullet) # Add it to the scene tree
+	bullet.position = self.position # Set its position (adjust as needed)
+
 
 func get_input():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
