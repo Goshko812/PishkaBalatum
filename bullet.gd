f32b89c0e31eaf2686ea96c15f19690fc37c7a9f
Goshko812 <goshko812@gmail.com>
2023-11-06 00:29:28 +0200

    Still dealing with bullets man shit sucks

--- bullet.gd
+++ bullet.gd
@@ -1,11 +1,8 @@
 extends Area2D
 
-var speed = 750
+var speed = 1000 # Adjust this value to control bullet speed
 
-func _physics_process(delta):
-	position += transform.x * speed * delta
-
-func _on_Bullet_body_entered(body):
-	if body.is_in_group("mobs"):
-		body.queue_free()
-	queue_free()
+func _process(delta):
+	translate(Vector2(speed * delta, 0)) # Adjust direction if needed
+	if position.x > get_viewport_rect().size.x:
+		queue_free() # Destroy the bullet when it leaves the screen
