1315faf0108428fe755c47bb8b55c9864d33fdb3
Goshko812 <goshko812@gmail.com>
2023-11-06 19:01:40 +0200

    no more background stretching lol

--- Bullett.gd
+++ Bullett.gd
@@ -10,7 +10,7 @@ func launch(direction: Vector2, speed: float):
 func _physics_process(delta):
 	position += velocity * delta
 
-	if position.length() > 1000:
+	if position.length() > 4000:
 		queue_free()
 
 func _on_Bullet_body_entered(body):
