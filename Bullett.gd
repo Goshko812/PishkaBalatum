63f60bcd3bb79cd389252d9a56c7bfd84d0b016d
Goshko812 <goshko812@gmail.com>
2023-11-06 05:28:26 +0200

    made directional shooting and bullet collision

--- Bullett.gd
+++ Bullett.gd
@@ -11,3 +11,7 @@ func _physics_process(delta):
 	if position.length() > 1000:
 	#if position.x > 800 or position.x < 0 or position.y > 600 or position.y < 0:
 		queue_free()
+func _on_Bullet_body_entered(body):
+	if body.is_in_group("mobs"):
+		body.queue_free()
+		queue_free()
--- Player.gd
+++ Player.gd
@@ -3,17 +3,19 @@ extends CharacterBody2D
 @export var speed = 400
 var bullet_scene = preload("res://Bullet.tscn")
 
-func shoot():
+func shoot(direction: Vector2):
 	var bullet_instance = bullet_scene.instantiate()
 	get_parent().add_child(bullet_instance)
-	bullet_instance.global_position = global_position # Set initial position in global coordinates
-	bullet_instance.launch(Vector2(1, 0), 1000) # Set direction and speed as needed
+	bullet_instance.global_position = global_position
+	bullet_instance.launch(direction, 1000) # Set a suitable speed
 
 func get_input():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * speed
-	if Input.is_action_just_pressed("shoot"):
-		shoot()
+
+	# Pass the direction to the shoot() function only if there is movement
+	if input_direction != Vector2.ZERO and Input.is_action_just_pressed("shoot"):
+		shoot(input_direction)
 
 func _physics_process(delta):
 	get_input()
