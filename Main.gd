38e94971fde28f42108af2647544dbb22d5f596b
Goshko812 <goshko812@gmail.com>
2023-11-11 02:31:49 +0200

    added enemy spawning

--- Main.gd
+++ Main.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var enemy_scene = preload("res://Enemy.tscn")
+
 func _ready():
 	$Timer.start()
 
@@ -7,8 +9,11 @@ func _on_Timer_timeout():
 	spawn_enemy()
 
 func spawn_enemy():
-	if $Enemy:  
-		$Enemy.position = random_position()
+	if enemy_scene:  
+		var new_enemy = enemy_scene.instantiate()
+		new_enemy.position = random_position()
+		add_child(new_enemy)
+		print("enemy spawned")
 
 func random_position() -> Vector2:
 	return Vector2(randi() % 4000, randi() % 4000)
