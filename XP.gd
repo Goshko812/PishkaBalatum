63f1e7fed30c0cd75da5d4bc4ba08f38aebae21a
Val0u <valeriivanov2003@abv.bg>
2023-11-11 12:56:11 +0200

    xp

--- XP.gd
+++ XP.gd
@@ -1,5 +1,21 @@
 extends AnimatedSprite2D
 
+
+var player = preload("res://Player.gd")
+var move_speed : float = 100.0
+var is_moving : bool = false
+
+func _ready():
+	player = $"/root/World/Player" #Ne znam dali e taka na 100%
+
+func _process(delta):
+	if is_moving:
+		move_towards_player(delta)
+
+func move_towards_player(delta):
+	var direction = (player.global_position - global_position).normalized()
+	global_position += direction * move_speed * delta
+
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("player"):
-		queue_free()
+		is_moving = true
